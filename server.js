075d7ba13a2b3284d0db70bdc4cb36f0b91f6dc3
Ritcheydevs <Richard4codes.1998>
2019-03-24 01:13:11 +0100

    server update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const serveStatic = require("serve-static")
+const serveStatic = require("serve-static");
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+const port = 8919;
 app.listen(port);
